9e9ba23efb95ae46057b345f6a6191e7a6a0652c
SherifFuang <641517050@qq.com>
2018-11-22 13:57:44 +0800

    1

---
+++ 案例项目实战/py.project_Cameo/IOprocessing/IOprocessing/cameo.py
@@ -0,0 +1,57 @@
+﻿import cv2 
+import filters
+from managers import WindowManager, CaptureManager  #from import : 从车里把矿泉水拿出来，给我.而import : 把车给我。 在这则是从manager项目中读取win... Cap..两个类
+
+class Cameo(object):
+    def __init__(self):
+        # 创建一个窗口，并将键盘的回调函数传入
+        self._windowManager = WindowManager('Cameo', self.onKeypress)
+        # 告诉程序数据来自视频文件， 还镜面翻转效果
+        video_path = "C:\\Users\\admin\\Documents\\Visual Studio 2015\\Projects\\Testphoto\\FaceVideo.mp4"
+        self._captureManager = CaptureManager(capture=cv2.VideoCapture(video_path), previewWindowManager=self._windowManager, shouldMirrorPreview=True)
+        self._curveFilter = filters.BGRPortraCurveFilter()
+
+    def run(self):
+        """Run the main loop."""
+        self._windowManager.createWindow()
+        while self._windowManager.isWindowCreated:
+            # 这里的enterFrame就是告诉程序从视频中读取数据
+            self._captureManager.enterFrame()
+            # 下面的这个frame是原始帧数据，这里没有做任何修改，后面的教程会对这个帧数据进行修改
+            frame = self._captureManager._frame
+            if frame is not None:
+                
+                # TODO: Track faces (Chapter 3).
+                
+                filters.strokeEdges(frame, frame)
+                self._curveFilter.apply(frame, frame)
+
+            self._captureManager.exitFrame()
+            self._windowManager.processEvents()
+    
+            # exitFrame看起来是像是退出的意思，其实主要功能都是在这里方法里实现的，截屏、录像都是在这里
+            self._captureManager.exitFrame()
+            # 回调函数
+            self._windowManager.processEvents()
+    # 定义键盘的回调函数，用于self._windowManager.processEvents()的调用
+    def onKeypress(self, keycode):
+        '''
+        快捷键设置：
+        当按下“空格”键的时候，会进行抓屏。
+        当按下‘tab’键的时候，就开始或者停止录像。
+        当然相应的目录也会生成图片或者视频文件
+        '''
+        if keycode == 32:#space
+            # 截屏保存的文件名字
+            self._captureManager.writeImage('screenshot.png')
+        elif keycode == 9:#tab
+            if not self._captureManager.isWritingVideo:
+                # 告诉程序，录像保存的文件名字
+                self._captureManager.startWritingVideo('screen_record.avi')
+            else:
+                self._captureManager.stopWritingVideo()
+        elif keycode == 27: #escape
+            self._windowManager.destroyWindow()
+
+if __name__=="__main__":
+    Cameo().run()
--- 案例项目实战/py.project_Cameo/IOprocessing/IOprocessing/managers.py
+++ 案例项目实战/py.project_Cameo/IOprocessing/IOprocessing/managers.py
@@ -1,13 +1,15 @@
-#使用managers.CaptureManager提取视频流
-import numpy as np
-import cv2 as cv
+﻿import numpy 
+import cv2 
 import time 
-
+#使用managers.CaptureManager提取视频流
 # 增加要导入的包、构造函数和属性值
 class CaptureManager(object): 
-    def __init__(self, capture, previewWindowManager = None, shouldMirrorPreview = False):#这个怎么会有这么多属性值，哪里来的
+# 类变量前加 _ 代表将变量设置保护变量，只有类对象和子类才能访问
+# 类变量前加 __ 代表将变量设置私有变量，只有类对象才能访问
+    def __init__(self, capture, previewWindowManager = None, shouldMirrorPreview = False):#高级的I/O流接口，可以获取帧并分配输出，shouldMirrorPreview是否镜像，previewWindowManager退出frame之前是否要在窗口中显示帧属性
+       #CaptureManager类被定义了三个参数，分别为输入视频流，输出的窗口以及镜像参数。
         self.previewWindowManager = previewWindowManager
-        self.shouldMirrorPreview = shouldMirrorPreview
+        self.shouldMirrorPreview  =  shouldMirrorPreview
         self._capture = capture
         self._channel = 0
         self._enteredFrame = False
@@ -20,62 +22,89 @@ class CaptureManager(object):
         self._framesElapsed = int(0)
         self._fpsEstimate = None
        
+    # 设置窗口只读属性
     @property
-    def channel(self):
+    def channel(self):#获取通道属性
         return self._channel
+    # 设置可写属性
     @channel.setter
     def channel(self, value):
         if self._channel != value:
             self._channel = value
             self._frame = None
+            
+    # 设置帧只读属性
     @property
     def frame(self):
         if self._enteredFrame and self._frame is None:
             _, self._frame = self._capture.retrieve()
         return self._frame
+    
+    # 设置只读属性
     @property
     def isWritingImage(self):
         return self._imageFilename is not None
     @property
     def isWritingVideo(self):
         return self._videoFilename is not None
- 
-    def enterFrame(self):
+
+
+ # 启动摄像头录制功能
+    def enterFrame(self):#进入帧，只能同步获取一帧
+        #capture the next frame if any，but first check whether that any previous frame was exited
+        # 第一步，检查是否有之前帧存在
         assert not self._enteredFrame, 'previous enterFrame() had no matching exitFram()'
         if self._capture is not None:
             self._enteredFrame = self._capture.grab()
-    def exitFrame(self):
+
+# 程序里最复杂的就是这方法了，担负了视频显示、视频录制、截屏保存的功能。
+    def exitFrame(self):#退出帧
+        """draw to the window write to files , Release the frame"""   
+        #check whether any grabbed frame is retrievable. the getter may retrieve and cache frame 
+        # 检查是否有捕获的帧是可获取的
+        # getter可能获取并缓存帧
         if self.frame is None:
             self._enteredFrame = False
             return
+         # 更新帧估计和相关变量
         if self._framesElapsed == 0:
-            self._startTime = time.time()
+            self._startTime = time.time()#time.time()是python 中专门用来估测帧速率的
         else:
             timeElapsed = time.time() - self._startTime
             self._fpsEstimate = self._framesElapsed / timeElapsed
         self._framesElapsed += 1
-        if self.previewWindowManager is not None:
+        if self.previewWindowManager is not None: #通过窗口管理器（如果有）显示图像
             if self.shouldMirrorPreview:
                 mirroredFrame = numpy.fliplr(self._frame).copy()
                 self.previewWindowManager.show(mirroredFrame)
             else:
                 self.previewWindowManager.show(self._frame)
+        #write to the image file ,if any 写入图像到文件
         if self.isWritingImage:
             cv2.imwrite(self._imageFilename, self._frame)
             self._imageFilename = None
+        #write to the video file, if any 录像生成
         self._writerVideoFrame()
+        #release the frame 
         self._frame = None
         self._enteredFrame = False
  
+
+        # 设置图片文件名
     def writeImage(self, filename):
         self._imageFilename = filename
+        # 开始录制
     def startWritingVideo(self, filename, encoding = cv2.VideoWriter_fourcc('I', '4', '2', '0')):
+        
         self._videoFilename = filename
         self._videoEncoding = encoding
+        # 开始录制
     def stopWritingVideo(self):
         self._videoFilename = None
         self._videoEncoding = None
         self._videoWriter = None
+        
+    # 录制视频
     def _writerVideoFrame(self):
         if not self.isWritingVideo:
             return
@@ -91,4 +120,32 @@ class CaptureManager(object):
             self._videoWriter = cv2.VideoWriter(self._videoFilename, self._videoEncoding,fps,size)
             self._videoWriter.write(self._frame)
           
-
+# 使用managers.WindowManager抽象窗口和键盘
+# 创建界面管理类
+class WindowManager(object):
+    # 实现支持键盘事件
+    def __init__(self, windowName, keypressCallback = None):
+        self.keypressCallback = keypressCallback
+        self._windowName = windowName   
+        self._isWindowCreated = False
+    # 设置只读属性
+    @property
+    def isWindowCreated(self):
+        return self._isWindowCreated
+    # 创建窗口
+    def createWindow(self):
+        cv2.namedWindow(self._windowName)
+        self._isWindowCreated = True
+    # 显示窗口
+    def show(self, frame):
+        cv2.imshow(self._windowName, frame)
+    # 注销窗口
+    def destoryWindow(self):
+        cv2.destroyWindow(self._windowName)
+        self._isWindowCreated = False
+     # 执行键盘操作的回调函数
+    def processEvents(self):
+        keycode = cv2.waitKey(1)
+        if self.keypressCallback is not None and keycode != -1:
+            keycode &= 0xFF
+            self.keypressCallback(keycode)
